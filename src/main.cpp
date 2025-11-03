3331266331a5d60e5ce0dea724f7709c91c98dc5
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-11-03 23:07:02 +0200

    Add LED toggle functionality via web interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <uri/UriBraces.h>
 
 #define WIFI_SSID "Wokwi-GUEST"
 #define WIFI_PASSWORD ""
@@ -7,8 +8,16 @@
 
 WebServer server(80);
 
+const int LED1 = 26;
+const int LED2 = 27;
+
+bool led1State = false;
+bool led2State = false;
+
 void setup(void) {
   Serial.begin(115200);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD, WIFI_CHANNEL);
   Serial.print("Connecting to WiFi ");
@@ -21,6 +30,25 @@ void setup(void) {
 
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  server.on(UriBraces("/toggle/{}"), []() {
+    String led = server.pathArg(0);
+    Serial.print("Toggle LED #");
+    Serial.println(led);
+
+    switch (led.toInt()) {
+      case 1:
+        led1State = !led1State;
+        digitalWrite(LED1, led1State);
+        break;
+      case 2:
+        led2State = !led2State;
+        digitalWrite(LED2, led2State);
+        break;
+    }
+
+    server.send(200, "text/plain", "OK");
+  });
 
   server.begin();
   Serial.println("HTTP server started");
